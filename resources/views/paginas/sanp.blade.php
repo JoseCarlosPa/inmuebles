dc37f47ddb5510305ad6dbf7df4ac0093d33d354
Jose Carlos <A01702828@itesm.mx>
2020-08-31 08:56:48 -0500

    Pagina sanp terminada

---
+++ resources/views/paginas/sanp.blade.php
@@ -0,0 +1,355 @@
+@extends('layouts.app')
+@section('title','Insignia | Contacto ')
+@section('content')
+    <div class="bg-inicio">
+        <img src="{{asset('images/pags/nv1.png')}}" class="fondo-inicio" alt="fondo inicio">
+    </div>
+    <br><br>
+    <div class="container quienes-main">
+        <div class="row">
+            <div class="col-sm-12">
+                <h4>FILOSOFÍA</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row text-left">
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4> ¿QUIENES SOMOS? </h4>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row text-nova">
+                    <div class="col-sm-12">
+                        <p>Somos una empresa profesional que brinda un servicio inmobiliario integral.</p>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4>NUESTRO SUEÑO </h4>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row text-nova">
+                    <div class="col-sm-12">
+                        <p> Llegar a ser una empresa plenamente humana, altamente rentable y ejemplar para México y
+                            Latinoamérica.</p>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4>NUESTRA CAUSA </h4>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row text-nova">
+                    <div class="col-sm-12">
+                        <p>Brindar un servicio inmobiliario que trascienda y nos permita contribuir a formar familias
+                            felices.</p>
+                    </div>
+                </div>
+
+                <br>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4>NUESTROS VALORES </h4>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ul>
+                            <li class="lista-text-nova">AMOR POR EL SERVICIO APASIONADO</li>
+                            <li class="lista-text-nova">INTEGRIDAD PURA</li>
+                            <li class="lista-text-nova">IMAGEN IMPECABLE</li>
+                            <li class="lista-text-nova">INNOVACIÓN PROACTIVA</li>
+                            <li class="lista-text-nova">DESARROLLO TRASCENDENTE</li>
+                        </ul>
+
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-sm-6">
+                <h4>OBJETIVOS</h4>
+                <hr>
+                <p class="text-nova">
+                    Fomentamos la relación comercial en nuestros desarrollos industriales.
+                    <br><br>
+                    Somos una empresa regional con presencia en Guanajuato, Jalisco, Querétaro, San Luis Potosí y BCS.
+                    <br><br>Ofrecemos el mismo precio por m2 en cualquier punto geográfico dentro de los desarrollos
+                    industriales.
+                    <br><br>Potencializamos el crecimiento industrial de la región con clústeres enfocados a MIPYMES.
+                    <br><br>Contamos con zonas logísticas para movimientos en lugares estratégicos.
+                    <br><br>Impulsamos el desarrollo económico e industrial en zonas conurbanadas.</p>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>INTRODUCCIÓN</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/img-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="cols-m-12 text-nova text-left">
+                <br><br>
+                <p>Nace de la necesidad de contar con espacios dedicados a la industria MIPYMES en el importante
+                    corredor industrial centro-bajío del país (Guanajuato, Querétaro, Aguascalientes, San Luis Potosí y
+                    BCS). Espacios que satisfacen las necesidades de sala de exhibición (showroom) y/o almacenamiento
+                    (warehouse), complementarios al entorno empresarial actual. Industriametal mecánica para armadora de
+                    autos (Mazda, Honda, Toyota), electrónica, aeronáutica y su respectiva cadena de suministro y
+                    proveeduría.
+                    <br><br>
+                    Servicios, centros de distribución, logística, paquetería y un sin número de giros
+                    industriales y comerciales que perfectamente encajan en nuestro modelo de negocio.
+                    <br><br>Por su privilegiada ubicación geográfica, NOVOTECH APASEO es una opción excelente en
+                    términos de logística
+                    y rentabilidad.
+                    <br><br>Dotada de servicios, su empresa gozará de una óptima relación costo-beneficio.</p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>UBICACIÓN</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/ubic-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>CORREDOR INDUSTRIAL</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/coore-no1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>LOTIFICACIÓN</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/lot-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>SERVICIOS</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 quienes-main text-left">
+                        <h4>AGUA</h4>
+                        <p class="text-nova">n 50 m3 por lote</p>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 quienes-main text-left">
+                        <h4>SERVICIOS SANITARIOS </h4>
+                        <p class="text-nova">n Drenaje</p>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 quienes-main text-left">
+                        <h4>VIALIDADES </h4>
+                        <p class="text-nova">n Servidumbre industrial 17 cm n Carpeta asfáltica 7 cm en n tramos rectos
+                            n Concreto hidráulico 20 cm en zonas de giro</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 quienes-main text-left">
+                        <h4>ENERGÍA ELÉCTRICA</h4>
+                        <p class="text-nova"> n 15 kva por lote (electricidad 13.5 kW)</p>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 quienes-main text-left">
+                        <h4>OTROS</h4>
+                        <p class="text-nova"> n Barda perimetral de block con cara de piedra n Caseta con control de
+                            acceso n Vigilancia n CCTV n Alumbrado led</p>
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>LOTE EJEMPLO</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/lote-ej-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>CONSTRUCCIÓN DE NAVES</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-6 text-center">
+                <img src="{{asset('images/pags/construccion-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+
+            <div class="col-sm-6 text-left">
+                <p class="text-nova">
+                    Nuestra prioridad es consolidar tu inversión a través de la edificación (construcción) de espacios
+                    adecuados a la demanda creciente del Corredor Industrial del Bajío. La tendencia del sector
+                    industrial del Bajío se consolida en naves industriales de 5,000 m2 o mayores, construidas a la
+                    medida y una incierta demanda de espacios inferiores de 5,000 m2.
+                    <br><br>EL MERCADO ESPECÍFICO PARA
+                    ALMACENAMIENTO
+                    <br><br>ESPECIFICACIONES (NAVE TIPO) <br>
+                    Las naves se diseñan de acuerdo a su presupuesto y
+                    necesidades, siendo el objetivo satisfacer las especificaciones técnicas a cada uno de los clientes
+                    en el sector metalmecánica, logística para otorgar la rigidez necesaria.</p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col-sm-12 text-left">
+                <p class="text-nova">
+                    n Modelo Base con muro de block liso hueco 12-20-40 en color natural 2.5m de altura. Varillas
+                    ahogadas por el centro del block para otorgar rigidez necesaria. n Por requisición estética se puede
+                    utilizar block caravista de diversos colores (arena, rojo, negro). Firme de 15 cm de espesor de
+                    concreto tipo MR-35 reforzado con tecnólogas de fibra metálica ondulada con una capacidad de carga
+                    de 5-6 ton/m2.<br> n Acabado pulido con juntas de control y de construcción con sello autonivelante.
+                    n
+                    La capacidad de carga del firme puede variar según las especificaciones del cliente.</p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+
+            <div class="col-sm-6 text-left">
+                <p class="text-nova">
+                    Perfil IPR de acero A-36, sección variable con aplicación de primario y pintura de acabado en color
+                    blanco tanto en columnas como en vigas.
+                    <br><br> Montenes de 8" calibre 10 acabados blanco y fondo gris.<br>
+                    Lámina pintro KR-18 calibre 24 color blanco y fondo gris intercalada con lámina acrílica
+                    transparente compatible con KR-18 para proporcionar luz natural dentro de la nave.
+                    <br><br> Se pueden usar
+                    diferentes tipos de lámina, para mejor aislamiento térmico y acústico.<br><br> Canalón de lámina
+                    pinto color
+                    blanco calibre 24</p>
+            </div>
+            <div class="col-sm-6 text-center">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <img src="{{asset('images/pags/vg1-nov1.png')}}" alt="imagen novatec" width="70%">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <img src="{{asset('images/pags/vg2-nov2.png')}}" alt="imagen novatec" width="70%">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <img src="{{asset('images/pags/vg3-nv1.png')}}" alt="imagen novatec" width="70%">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="text-nova text-left">Contra-flameos de perfil angular de 1 1/2 para unir a los montenes entre si y
+                    proporcionar rigidez de la estructura.
+                    <br><br>
+                    Contra-venteos de perfil redondo liso con diámetro variable,
+                    para proporcionar una estructura rígida.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>CLIENTES</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/clientes-nv1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>GALERÍA</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/gal-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 quienes-main">
+                <h4>CENTRO DE NEGOCIOS</h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <img src="{{asset('images/pags/neg-nov1.png')}}" alt="imagen novatec" width="100%">
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <a href=""><button class="button-contacto"> Descargar PDF</button></a>
+            </div>
+        </div>
+        <br><br>
+
+    </div>
+
+
+@endsection
